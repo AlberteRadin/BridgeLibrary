ab5321993a89b5563329fbebb0aa6a655de4f1a7
AlberteRadin <alberte@radin.dk>
2025-09-24 12:51:58 +0200

    Add project files.

---
+++ BridgeLibrary/Car.cs
@@ -0,0 +1,38 @@
+﻿namespace BridgeLibrary
+{ 
+    public class Car
+    {
+        /// <summary>
+        /// En Car klasse, med 2 properties: LicensePlate og Date
+        /// </summary>
+        public string LicensePlate { get; }
+        public DateTime Date { get; }
+
+        public Car(string licensePlate, DateTime date)
+        {
+            LicensePlate = licensePlate;
+            Date = date;
+        }
+
+        /// <summary>
+        /// En metode der returnerer prisen for en bil der bruger broen
+        /// </summary>
+        /// <returns>Den returnere 250 Kr</returns>
+        public double Price()
+        {
+            return 250;
+        }
+        
+        /// <summary>
+        /// En metode der returnere typen af køretøj som Car
+        /// </summary>
+        /// <returns>Returnere en string Car</returns>
+        public string VehicleType()
+        {
+            return "Car";  
+        }
+
+
+
+    }
+}
---
+++ BridgeLibraryTests/CarTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BridgeLibrary;
+using System.Diagnostics.CodeAnalysis;
+namespace BridgeLibrary.Tests
+{
+    [TestClass()]
+    [ExcludeFromCodeCoverage]
+    public class CarTests
+    {
+        [TestMethod()]
+        public void PriceTest()
+        {
+            var car = new Car("FB30360", DateTime.Now);
+
+            double Price = car.Price();
+
+            Assert.AreEqual(250, Price);
+        }
+       [TestMethod()]
+        public void VehicleTypeTest()
+        {
+            var car = new Car("FB30360", DateTime.Now);
+
+            string VehicleType = car.VehicleType();
+
+            Assert.AreEqual("Car", VehicleType);
+        }
+    }
+}
