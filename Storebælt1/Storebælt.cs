0df1d0cd06c97483431a01a3f36625758663e805
AlberteRadin <alberte@radin.dk>
2025-09-29 13:04:41 +0200

    Lavet en storebæltLibrary. + test

---
+++ Storebælt1/Storebælt.cs
@@ -0,0 +1,29 @@
+﻿using BridgeLibrary;
+namespace StorebæltTicketLibrary
+{
+    public class Storebælt : Vehicle
+    {
+        public Storebælt(string licensePlate, DateTime date, bool brobizz)
+          : base(licensePlate, date, brobizz)
+        {
+        }
+
+        public override double Price()
+        {
+            double basePrice = 250;
+
+            // Apply weekend discount
+            if (Date.DayOfWeek == DayOfWeek.Saturday || Date.DayOfWeek == DayOfWeek.Sunday)
+            {
+                basePrice *= 0.85; // 15% discount on weekends
+            }
+
+            if (Brobizz)
+                return basePrice * 0.9;
+           
+            return basePrice;
+        }
+
+
+    }
+}
---
+++ StorebæltTicketLibraryTests/StorebæltTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StorebæltTicketLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorebæltTicketLibrary.Tests
+{
+    [TestClass()]
+    public class StorebæltTests
+    {
+        [TestMethod()]
+        public void Price_Weekday_NoDiscount()
+        {
+            var ticket = new Storebælt("AB12345", new DateTime(2025, 9, 29), false); // Monday
+            Assert.AreEqual(250, ticket.Price());
+        }
+
+        [TestMethod()]
+        public void Price_Weekend_DiscountOnly()
+        {
+            var ticket = new Storebælt("AB12345", new DateTime(2025, 10, 4), false); // Saturday
+            Assert.AreEqual(212.5, ticket.Price());
+        }
+
+        [TestMethod()]
+        public void Price_Weekday_BrobizzOnly()
+        {
+            var ticket = new Storebælt("AB12345", new DateTime(2025, 9, 29), true); // Monday
+            Assert.AreEqual(225, ticket.Price());
+        }
+
+        [TestMethod()]
+        public void Price_Weekend_And_Brobizz_BothDiscounts()
+        {
+            var ticket = new Storebælt("AB12345", new DateTime(2025, 10, 5), true); // Sunday
+            Assert.AreEqual(191.25, ticket.Price());
+        }
+    }
+}
